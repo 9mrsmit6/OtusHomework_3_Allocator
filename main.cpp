c0467291cc93edc11d94a84cf138fccdad5979bf
Ahrameshin Grigoriy <9mrsmit6@gmail.com>
2023-02-01 10:59:43 +0300

    add CI

--- main.cpp
+++ main.cpp
@@ -54,6 +54,7 @@ int main()
 
     myAllocatorMap[5]=321;
     copyMap[5]=123;
+    std::cout<<std::endl;
 
     printMap(copyMap,  "COPY");
     printMap(myAllocatorMap,  "MYC ");
@@ -65,6 +66,8 @@ int main()
     auto testMap=std::move(myAllocatorMap);
     printMap(testMap,  "MOVE");
     printMap(myAllocatorMap,  "MYM ");
+    std::cout<<std::endl;
+
     myAllocatorMap[5]=567;
     testMap[5]=765;
 
@@ -74,7 +77,7 @@ int main()
     std::cout<<"----------------------------"<<std::endl;
 
 
-
+    std::cout<<"------- MY CONTEINER -------"<<std::endl;
 
     TestConteiner<int> myConteiner;
     for(int i=0;i!=10;i++)
@@ -92,94 +95,6 @@ int main()
         else{break;}
     }
     std::cout<<std::endl;
-
-
-
-
-
-
-//    std::allocator<TestContainerNode<int>> alloc;
-
-
-//    auto v=alloc.allocate(1);
-//    alloc.construct(v,123);
-
-//    TestContainerNode<int>& vax= *v;
-
-//    int* v1111 = new int(123);
-
-//    new(v1111) int(123);
-
-//    (*v1111)++;
-
-
-
-
-
-
-
-
-
-//    std::vector<int, SimpleAllocator<int,AllocatorConfig> > vector;
-
-
-//    auto m = std::map<int,float,std::less<int>,	SimpleAllocator<std::pair<	const int, float> , AllocatorConfig> >{};
-
-
-//    for (int i = 0; i < 10; ++i) {
-//        vector.push_back(i);
-//        m[i]=i;
-//    }
-
-//    for (const auto& v : vector)
-//    {
-//        std::cout << v << ' ';
-//    }
-//    std::cout << std::endl;
-//    std::cout << "MAP: ";
-//    for (int i=0;i!=10; i++)
-//    {
-//        std::cout << m[i] << ' ';
-//    }
-//    std::cout << std::endl;
-
-//    auto other = vector;
-
-//    for (const auto& v : other) {
-//        std::cout << v << ' ';
-//    }
-//    std::cout << std::endl;
-
-
-//    auto value=128;
-
-//    auto ptr=std::make_unique<int>(value);
-
-//    std::list<std::unique_ptr<int>> list;
-
-//    list.push_front(std::make_unique<int>(value));
-
-
-
-//    auto base=std::bitset<16>(0b1110000011101111);
-//    std::cout<<base<<std::endl;
-
-
-
-////   auto index=Utils::getAllocationIndex<16>(base, 6);
-////   if(!index)
-////   {
-////       std::cout<<"NO"<<std::endl;
-////   }
-////   else
-////   {
-////       std::cout<<"OK:"<<index.value()<<std::endl;
-
-////   }
-
-
-
-
 
 
 
